53c839703f3726cdae6282cdafd98408da21f41b
PaSawi <sawicki.patryk@gmail.com>
2023-02-25 22:07:08 +0100

    Add showng student's data

--- exercises/04_university-db/src/test.cpp
+++ exercises/04_university-db/src/test.cpp
@@ -6,7 +6,29 @@ struct DatabaseTest : ::testing::Test {
     Database db;
 };
 
-TEST_F(DatabaseTest, CanAddStudentToDb_Req1_Req2)
+// TEST_F(DatabaseTest, CanAddStudentToDb_Req1_Req2)
+// {
+//     Student adam {
+//         "Adam",
+//         "Kowalski",
+//         "ul. Dobra 134, 00-200 Warszawa",
+//         123456,
+//         "11223344556",
+//         Gender::Male
+//     };
+
+//     EXPECT_TRUE(db.add(adam));
+//     EXPECT_FALSE(db.add(adam));
+// };
+
+TEST_F(DatabaseTest, DisplayEmptyDb)
+{
+    auto content = db.show();
+    auto expected = "";
+    EXPECT_EQ(content, expected);
+};
+
+TEST_F(DatabaseTest, DisplayNonEmptyDb)
 {
     Student adam {
         "Adam",
@@ -16,31 +38,10 @@ TEST_F(DatabaseTest, CanAddStudentToDb_Req1_Req2)
         "11223344556",
         Gender::Male
     };
+    db.add(adam);
+    // check adding the same person
 
-    EXPECT_TRUE(db.add(adam));
-    EXPECT_FALSE(db.add(adam));
-};
-
-TEST_F(DatabaseTest, DisplayEmptyDb)
-{
     auto content = db.show();
-    auto expected = "";
+    auto expected = "Adam Kowalski; ul. Dobra 134, 00-200 Warszawa; 123456; 11223344556; Male";
     EXPECT_EQ(content, expected);
-};
-
-// TEST(DisplayDb, DisplayNonEmptyDb)
-// {
-//     Database db;
-
-//     Student adam {
-//         "Adam",
-//         "Kowalski",
-//         "ul. Dobra 134, 00-200 Warszawa",
-//         123456,
-//         "11223344556",
-//         Gender::Male
-//     };
-//     EXPECT_TRUE(db.add(adam));
-
-//     db.display();
-// }
+}
