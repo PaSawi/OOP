f0566a94f069738d45a8b938e897e6a9caa47813
PaSawi <sawicki.patryk@gmail.com>
2023-03-06 09:56:54 +0100

    added clas Item

--- exercises/05_cargointer/cargoint.cpp
+++ exercises/05_cargointer/cargoint.cpp
@@ -28,7 +28,7 @@ public:
 
     size_t getPrice() const override
     {
-        if (spoilByTime_ >= expiryDate_)
+        if (spoilByTime_ > expiryDate_)
             return 0;
         return static_cast<size_t>(basePrice_ * ((float)(expiryDate_ - spoilByTime_)) / expiryDate_);
     }
@@ -57,14 +57,14 @@ public:
     size_t getExpiryDate() const { return expiryDate_; }
 
 private:
-    size_t spoilByTime_;
+    size_t spoilByTime_ { 0 };
     size_t expiryDate_;
 };
 
 //**************************************************************************//
 class Alcohol : public Cargo {
 public:
-    Alcohol(std::string& name, size_t amount, size_t basePrice, size_t alcoholPercentage);
+    Alcohol(const std::string& name, size_t amount, size_t basePrice, size_t alcoholPercentage);
     ~Alcohol() override = default;
 
     size_t getPrice() const override { return basePrice_ * alcoholPercentage_ / 96; }
@@ -84,7 +84,7 @@ public:
         return *this;
     }
 
-    size_t getAlcoholPercentage() const {return alcoholPercentage_;}
+    size_t getAlcoholPercentage() const { return alcoholPercentage_; }
 
 private:
     size_t alcoholPercentage_;
@@ -92,5 +92,37 @@ private:
 
 //****************************************************************************************************
 
-class Item : Cargo {
+class Item : public Cargo {
+public:
+    enum class Rarity {
+        common = 1,
+        rare = 3,
+        epic = 6,
+        legendary = 10
+    };
+
+    Item(const std::string& name, size_t amount, size_t basePrice, Rarity rarity);
+    ~Item() override = default;
+
+    // override from Cargo
+    size_t getPrice() const override { return basePrice_ * static_cast<int>(rarity_); }
+    std::string getName() const override { return name_ ;}
+    size_t getAmount() const override { return amount_ ;}
+    size_t getBasePrice() const override { return basePrice_ ;}
+
+    Cargo& operator+=(size_t amount)
+    {
+        amount_ += amount;
+        return *this;
+    };
+    Cargo& operator-=(size_t amount)
+    {
+        amount_ -= amount;
+        return *this;
+    };
+
+    Rarity getRarity() const { return rarity_; }
+
+private:
+    Rarity rarity_;
 };
